f34a92bc380b0aec153f2d1cbf488b00de751654
Anjali Vishwakarma <92050489+Anjali-987-vish@users.noreply.github.com>
2023-04-18 08:27:23 +0530

    Add files via upload

---
+++ Day5(Arrow functions).js
@@ -0,0 +1,16 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    var x = function(n){
+        if(n%2==0)
+            return n*2;
+        else
+            return n*3;
+    }
+    var newArray = nums.map(x);
+    return newArray;
+}
